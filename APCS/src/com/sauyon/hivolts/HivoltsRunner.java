f164aef23ed569c8f80a2ee5a235b51c5b6ae3df
Sauyon <git@sauyon.com>
2012-09-16 17:57:27 -0700

    Commit because i feel like it

--- APCS/src/com/sauyon/hivolts/HivoltsRunner.java
+++ APCS/src/com/sauyon/hivolts/HivoltsRunner.java
@@ -1,19 +1,17 @@
 package com.sauyon.hivolts;
 
+import java.awt.Dimension;
+
 import javax.swing.JFrame;
 
 public class HivoltsRunner
 {
 	public static void main(String[] args)
 	{
-		Hivolts h = new Hivolts();
+		HivoltsFrame hf = new HivoltsFrame();
 		
-		JFrame f = new JFrame();
-		
-		f.add(h);
-		f.setSize(384,414);
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setResizable(false);
-		f.setVisible(true);
+		hf.setMinimumSize(new Dimension(384,414));
+		hf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		hf.setVisible(true);
 	}
 }
